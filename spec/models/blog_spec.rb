e3020d8753510491f39839b8e3bff5110e8aee7f
Babin Bohora <bohorababin1@gmail.com>
2022-04-04 10:13:23 +0900

    test for blog model added

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe Blog, type: :model do
 
   describe '#initialize' do
 
-    let(:attri) {puts "horllo"
-       {title: "Title", url: "www.url.com", languages: ["rails", "rspec"] } }
+    let(:attri) { {title: "Title", url: "www.url.com", languages: ["rails", "rspec"] } }
 
     context "valid params" do
       it 'returns a valid blog with languages' do
@@ -23,7 +22,19 @@ RSpec.describe Blog, type: :model do
     end
 
     context "invalid params" do
-      
+      it 'returns a invalid blog without title' do
+        attri[:title] = ""
+        blog = Blog.new(attri)
+        blog.valid?
+        expect(blog.errors.messages).to eq( {title: ["can't be blank"]})
+      end
+
+      it 'returns a invalid blog without url' do
+        attri[:url] = ""
+        blog = Blog.new(attri)
+        blog.valid?
+        expect(blog.errors.messages).to eq({ url: ["can't be blank"]})
+      end
     end
   end
 end
