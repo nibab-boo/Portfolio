88695386151c72f200a8b50b9cea5469cfffb1d2
Babin Bohora <bohorababin1@gmail.com>
2022-04-04 19:17:46 +0900

    factory for blog created. Blog-m-spec refactored with factory

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -4,34 +4,27 @@ RSpec.describe Blog, type: :model do
 
 
   describe '#initialize' do
-
-    let(:attri) { {title: "Title", url: "www.url.com", languages: ["rails", "rspec"] } }
-
     context "valid params" do
       it 'returns a valid blog with languages' do
-        blog = Blog.new(attri)
-        p blog
+        blog = build(:blog)
         expect(blog.valid?).to eq(true)
       end
 
       it 'returns a valid blog without languages' do
-        attri[:languages] = []
-        blog = Blog.new(attri)
+        blog = build(:blog, languages: [])
         expect(blog.valid?).to eq(true)
       end
     end
 
     context "invalid params" do
       it 'returns a invalid blog without title' do
-        attri[:title] = ""
-        blog = Blog.new(attri)
+        blog = build(:blog, title: "")
         blog.valid?
         expect(blog.errors.messages).to eq( {title: ["can't be blank"]})
       end
 
       it 'returns a invalid blog without url' do
-        attri[:url] = ""
-        blog = Blog.new(attri)
+        blog = build(:blog, url: "")
         blog.valid?
         expect(blog.errors.messages).to eq({ url: ["can't be blank"]})
       end
---
+++ test/factories/blogs.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :blog do
+    title { "Title" }
+    url { "www.url.com" }
+    languages { [ "rails", "rspec" ] }
+  end
+end
